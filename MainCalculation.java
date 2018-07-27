b566c76a73d783fb22a0ed7474373f1e08552e47
BlueWind <zero18030@gmail.com>
2018-07-27 16:09:19 +0900

    function end #計算関数の完成 + 動作確認済み

--- MainCalculation.java
+++ MainCalculation.java
@@ -15,21 +15,21 @@ public class MainCalculation extends Application {
     String[] rank_Strings = { new String("S"), new String("A"), new String("B"), new String("C"), new String("D"),
             new String("F"), new String("-") };
     ChoiceBox[] subChoiceBoxs = {
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")),
-            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "-")) };
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")),
+            new ChoiceBox<String>(FXCollections.observableArrayList("S", "A", "B", "C", "D", "E", "F", "履修なし")) };
     ChoiceBox[] subNumChoiceBoxs = { new ChoiceBox<String>(FXCollections.observableArrayList("1", "2", "3", "4")),
             new ChoiceBox<String>(FXCollections.observableArrayList("1", "2", "3", "4")),
             new ChoiceBox<String>(FXCollections.observableArrayList("1", "2", "3", "4")),
@@ -45,6 +45,12 @@ public class MainCalculation extends Application {
             new ChoiceBox<String>(FXCollections.observableArrayList("1", "2", "3", "4")),
             new ChoiceBox<String>(FXCollections.observableArrayList("1", "2", "3", "4")),
             new ChoiceBox<String>(FXCollections.observableArrayList("1", "2", "3", "4")) };
+
+    String[] subNames = { new String("科学技術者倫理"), new String("ソフトウェア工学"), new String("分散システム"), new String("デジタル通信"),
+            new String("映像メディア"), new String("計算論"), new String("コンピュータアーキテクチャ実践"), new String("コンピュータグラフィックス"),
+            new String("符号と暗号"), new String("情報工学専門実験A・B") };
+    String[] subNums = { new String("2"), new String("2"), new String("2"), new String("2"), new String("2"),
+            new String("2"), new String("2"), new String("2"), new String("2"), new String("3") };
     Label lavel = new Label("結果");
     Button button = new Button("計算");
 
@@ -62,16 +68,17 @@ public class MainCalculation extends Application {
         mainGrid.setHgap(10);
         mainGrid.setVgap(10);
         for (int i = 0; i < subChoiceBoxs.length; i++) {
-            subChoiceBoxs[i].setValue("-");
-            subNumChoiceBoxs[i].setValue("1");
+            subChoiceBoxs[i].setValue("履修なし");
             if (i < 10) {
-                mainGrid.add(new TextField(), 0, i);
+                subNumChoiceBoxs[i].setValue(subNums[i]);
+                mainGrid.add(new TextField(subNames[i]), 0, i);
                 mainGrid.add(subChoiceBoxs[i], 1, i);
                 mainGrid.add(subNumChoiceBoxs[i], 2, i);
             } else {
+                subNumChoiceBoxs[i].setValue("1");
                 mainGrid.add(new TextField(), 3, i - 10);
                 mainGrid.add(subChoiceBoxs[i], 4, i - 10);
-                mainGrid.add(subNumChoiceBoxs[i], 2, i - 10);
+                mainGrid.add(subNumChoiceBoxs[i], 5, i - 10);
             }
         }
         mainGrid.add(lavel, 0, 11);
@@ -85,36 +92,38 @@ public class MainCalculation extends Application {
         int num = 0;
         int score = 0;
         for (int i = 0; i < subChoiceBoxs.length; i++) {
+            int subNum = Integer.parseInt(subNumChoiceBoxs[i].getValue().toString());
+            System.out.println("Sub:" + subNum);
             switch (subChoiceBoxs[i].getValue().toString()) {
             case "S":
-                num += (int) subNumChoiceBoxs[i].getValue();
-                score += 4 * (int) subNumChoiceBoxs[i].getValue();
+                num += subNum;
+                score += 4 * subNum;
                 break;
             case "A":
-                num += (int) subNumChoiceBoxs[i].getValue();
-                score += 3 * (int) subNumChoiceBoxs[i].getValue();
+                num += subNum;
+                score += 3 * subNum;
                 break;
             case "B":
-                num += (int) subNumChoiceBoxs[i].getValue();
-                score += 2 * (int) subNumChoiceBoxs[i].getValue();
+                num += subNum;
+                score += 2 * subNum;
                 break;
             case "C":
-                num += (int) subNumChoiceBoxs[i].getValue();
-                score += 1 * (int) subNumChoiceBoxs[i].getValue();
+                num += subNum;
+                score += 1 * subNum;
                 break;
             case "D":
-                num += (int) subNumChoiceBoxs[i].getValue();
-                score += 0 * (int) subNumChoiceBoxs[i].getValue();
+                num += subNum;
+                score += 0 * subNum;
                 break;
             case "E":
-                num += (int) subNumChoiceBoxs[i].getValue();
-                score += 0 * (int) subNumChoiceBoxs[i].getValue();
+                num += subNum;
+                score += 0 * subNum;
                 break;
             case "F":
-                num += (int) subNumChoiceBoxs[i].getValue();
-                score += 0 * (int) subNumChoiceBoxs[i].getValue();
+                num += subNum;
+                score += 0 * subNum;
                 break;
-            case "-":
+            case "履修なし":
                 break;
             default:
                 break;
@@ -123,7 +132,7 @@ public class MainCalculation extends Application {
         if (num != 0) {
             lavel.setText("QPAScore:" + (float) score / (float) num);
         } else {
-            lavel.setText("QPAScore:" + (float) score / (float) num + 1.0);
+            lavel.setText("QPAScore:" + 0);
         }
     }
 }
